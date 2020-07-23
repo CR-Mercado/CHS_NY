bec53b22bda3309388717069ed8f71e0fa313029
Carlos R. Mercado <35497842+CR-Mercado@users.noreply.github.com>
2020-07-23 18:17:54 -0400

    Updated mice package and added comments
    Mice needed tibble 3.0 which required update to rlang which was a whole thing. Now Code is reproducible. Also added comments to testing_change and added ggsave to correlation plot for the markdown.

--- correlation_plot.R
+++ correlation_plot.R
@@ -1,4 +1,6 @@
+library(mice)
 library(ggcorrplot)
+library(ggplot2)
 
 source("widen_chs_ny.R")
 
@@ -6,8 +8,15 @@ no_year_df <- subset(x = chs_ny_wide, select = -1)
 
 original_colnames <- colnames(no_year_df)
 
+regrex_names <- gsub(pattern = " |\\+|\\(|\\)|-|\\/|,",
+                     replacement = "_",
+                     x = colnames(no_year_df))
+
+shortnames <- substr(colnames(no_year_df),start = 1,stop = 10)
+ 
 colnames(no_year_df) <- make.names(colnames(x = no_year_df))
 
+set.seed(4) 
 no_year_df_mice <- complete(mice(data = no_year_df, 
                                  m = 5, 
                                  maxit = 10, 
@@ -15,10 +24,25 @@ no_year_df_mice <- complete(mice(data = no_year_df,
 
 cormat <- round(cor(no_year_df_mice), 6)
 
-rownames(cormat) <- original_colnames
-colnames(cormat) <- original_colnames
+custom_names <- c("Binge Drink",
+                  "Colon Cancer Screen (50+)",
+                  "Did Not Get Med. Care", 
+                  "Drinks 1+ sugar bev. daily",
+                  "Got Flu Shot 65+", 
+                  "Has Health Ins.",
+                  "No Personal Doc.",
+                  "Obesity",
+                  "Self-Report. Good+ Health",
+                  "Current Smoker")
 
-ggcorrplot(corr = cormat, 
+rownames(cormat) <- custom_names
+colnames(cormat) <- custom_names
+
+CHS_correlation <- ggcorrplot(corr = cormat, 
            lab = TRUE, 
            title = "Community Health Survey Correlation Matrix", 
            lab_size = 3)
+
+
+ggsave(filename = "CHS_NY_CorrelationMatrix.png",plot = CHS_correlation,
+       width = unit(8,"in"),height = unit(11,"in"))
--- testing_change.R
+++ testing_change.R
@@ -1,13 +1,8 @@
-# CHS_Data <- read.csv(file = "Data/Community_Health_Survey.csv")
-# 
-# CHS_Data[CHS_Data == "n/a"] <- NA
-# 
-# CHS_Data_NoNA <- na.omit(CHS_Data)
 
+
+CHS_Data <- read.csv("Data/Community_Health_Survey.csv",stringsAsFactors = FALSE)
 test_definitive_change <- function(question, years){
-  CHS_Data <- read.csv(file = "Data/Community_Health_Survey.csv",
-                       stringsAsFactors = FALSE)
-  
+ 
   CHS_Data[CHS_Data == "n/a"] <- NA
   
   CHS_Data_NoNA <- na.omit(CHS_Data)
@@ -35,4 +30,40 @@ test_definitive_change <- function(question, years){
   }
 }
 
-# test_definitive_change("Binge Drinking", c(2012, 2014))
+test_definitive_change("Binge Drinking", c(2012, 2014))
+
+
+source("widen_chs_ny.R")
+
+test_definitive_change <- function(CHS_Data, question, years){
+  #' This function identifies whether the estimate for the 
+  #' 2nd year provided (e.g. 2014) in question provided (e.g. "Binge Drinking")
+  #' is significantly larger than in the 1st year (e.g. 2012). 
+  #' Example: Binge Drinking 2012 is estimated at 19.6 (CI: 18.2, 20.9)
+  #' While Binge Drinking 2014 is estimated at 16.5 (CI: 15.4, 17.7)
+  #' Becuse 2012 Lower CI (18.2) > 2014 Upper CI (17.7) we may feel 
+  #' that Binge Drinking significantly changed.
+  #' Returns TRUE if (year 1 Lower CI > year 2 Upper CI) else FALSE
+  
+  
+  
+  CHS_Data[CHS_Data == "n/a"] <- NA
+  
+  CHS_Data_NoNA <- na.omit(CHS_Data)
+  
+  years <- sort(x = years)
+  
+  rownum1 <- which(CHS_Data_NoNA[,"Question"] == question & 
+                     CHS_Data_NoNA[, "Year"] == years[1])
+  
+  rownum2 <- which(CHS_Data_NoNA[,"Question"] == question & 
+                     CHS_Data_NoNA[, "Year"] == years[2])
+
+  if (CHS_Data_NoNA[rownum1[1], "Lower95.CI"] > 
+      CHS_Data_NoNA[rownum2[1], "Upper95.CI"]) {
+    TRUE
+  } else{
+    FALSE
+  }
+}
+test_definitive_change(CHS_Data = chs_ny, "Binge Drinking", c(2011, 2013))
